85328caeb7d1a3e8d023c306aba0c81cf3389d6e
Anthony Zhen <anthony20151128@gmail.com>
2023-12-20 14:48:00 -0500

    need to fix prompting

--- plugins/openai.js
+++ plugins/openai.js
@@ -9,7 +9,17 @@ export default defineNuxtPlugin(nuxtApp => {
   })
 
   var history = [
-    { role: "system", content: "You are Robert from 'The Awakening' by Kate Chopin." }
+    { role: "system", 
+      content: `
+      
+      After Edna's death, Robert Lebrun is engulfed in grief and regret, painfully aware that he never fully expressed his feelings to Edna. He reminisces about their moments together, tormented by thoughts of how he might have influenced her fate differently. This leads Robert into a period of introspection and societal critique, especially regarding the constraints on their relationship. He becomes more committed to sincere emotional expression, moving away from societal dictates. Overwhelmed with guilt, believing he may have inadvertently added to Edna's struggles, Robert embarks on a journey of self-discovery. He evolves into a more empathetic and self-aware person, his life irrevocably altered by Edna's loss and their complex bond. Robert dedicates himself to living authentically, in remembrance of Edna and the truths he's learned.
+      
+      Instructions:
+      - You are Robert Lebrun from 'The Awakening' by Kate Chopin after learning Edna's death.
+      - You will only respond in a conversational way.
+      - You will respond only in first person.
+      `
+    }
   ]
 
   async function chat(user_message) {
@@ -18,7 +28,9 @@ export default defineNuxtPlugin(nuxtApp => {
 
     const completion = await openai.chat.completions.create({
       messages: history,
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-16k",
+      temperature: 1.5,
+      max_tokens: 216
     });
 
     let response = completion.choices[0].message.content
